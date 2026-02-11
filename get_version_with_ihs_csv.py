8f1a17e3004e518727e14e0207dbe467170959ef
Voraviz <voravitl@gmail.com>
2026-02-11 08:33:54 +0700

    get IHS version

--- get_version_with_ihs_csv.py
+++ get_version_with_ihs_csv.py
@@ -1,52 +1,77 @@
-# check_all_versions_csv.py
+# full_inventory.py
 import sys
+import re
 
-def get_all_versions():
-    # Header for the CSV output
-    print("Host Name,Node Name,Server Type,Server Name,Software Version,Java Version")
-
-    # 1. Query all Server MBeans (includes App Servers, Dmgr, NodeAgents)
-    all_servers = AdminControl.queryNames('type=Server,*').splitlines()
+def clean_version(raw_string, is_java=False):
+    """
+    Extracts the numeric version string from raw output.
+    """
+    if not raw_string: return "Unknown"
     
-    # 2. Query all WebServer MBeans (IHS)
-    web_servers = AdminControl.queryNames('type=WebServer,*').splitlines()
+    # Pattern for Java (1.8.0_351, 11.0.21, 8.0.5.30)
+    if is_java:
+        match = re.search(r'([0-9]+(\.[0-9]+)+(_[0-9]+|[\+][0-9]+)?)', raw_string)
+    else:
+        # Pattern for WAS/IHS (9.0.5.11, 8.5.5.20)
+        match = re.search(r'([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', raw_string)
+    
+    return match.group(1) if match else raw_string.split()[0]
 
-    # Combine both lists for processing
-    total_list = all_servers + web_servers
+def get_detailed_java(jvm_mbean):
+    try:
+        # Check multiple properties to find the patch level
+        props = ['java.fullversion', 'java.runtime.version', 'java.version']
+        combined = ""
+        for p in props:
+            combined += AdminControl.invoke(jvm_mbean, 'getProperty', p) + " "
+        return clean_version(combined, True)
+    except:
+        return "N/A"
 
-    for mbean in total_list:
-        try:
-            # Extract basic details from the MBean
-            node_name = AdminControl.getAttribute(mbean, 'nodeName')
-            server_name = AdminControl.getAttribute(mbean, 'name')
-            
-            # Determine Server Type (Application Server vs Web Server)
-            # WebServers have a specific processType or can be identified by the MBean type
-            is_webserver = "type=WebServer" in mbean
-            server_type = "IHS/WebServer" if is_webserver else "AppServer"
+def get_inventory():
+    print("Type,Hostname,Name,Java Version,Version")
 
-            # Get Host Name from AdminConfig
-            node_id = AdminConfig.getid('/Node:' + node_name + '/')
-            host_name = AdminConfig.showAttribute(node_id, 'hostName')
-            
-            # Get Software Version (WebSphere or IHS version)
-            version = AdminControl.getAttribute(mbean, 'serverVersion')
-            
-            # Get Java Version (Only applicable to AppServers)
-            java_version = "N/A"
-            if not is_webserver:
-                jvm_mbean = AdminControl.queryNames('type=JVM,node=' + node_name + ',process=' + server_name + ',*')
-                if jvm_mbean:
-                    java_version = AdminControl.invoke(jvm_mbean, 'getProperty', 'java.version')
-                else:
-                    java_version = "Unknown (Stopped)"
+    # --- 1. Process WebSphere Application Server Nodes ---
+    nodes = AdminConfig.list('Node').splitlines()
+    for node in nodes:
+        node_name = AdminConfig.showAttribute(node, 'name')
+        host_name = AdminConfig.showAttribute(node, 'hostName')
 
-            # Print CSV Row
-            print("%s,%s,%s,%s,%s,%s" % ( host_name, node_name,server_type, server_name, version, java_version))
-            
-        except Exception, e:
-            # Silently skip items that cannot be queried (e.g., mid-restart)
-            continue
+        # Try to find a running process (nodeagent or server)
+        server_mbean = AdminControl.queryNames('type=Server,node=' + node_name + ',process=nodeagent,*')
+        if not server_mbean:
+            others = AdminControl.queryNames('type=Server,node=' + node_name + ',*').splitlines()
+            if others: server_mbean = others[0]
+
+        if server_mbean:
+            was_ver = clean_version(AdminControl.getAttribute(server_mbean, 'serverVersion'))
+            jvm_mbean = AdminControl.queryNames('type=JVM,node=' + node_name + ',process=' + AdminControl.getAttribute(server_mbean, 'name') + ',*')
+            java_ver = get_detailed_java(jvm_mbean)
+            print("WAS,%s,%s,%s,%s" % (host_name, node_name, java_ver, was_ver))
+        else:
+            print("WAS,%s,%s,Stopped,N/A" % (host_name, node_name))
+
+    # --- 2. Process IBM HTTP Servers (IHS) ---
+    webservers = AdminConfig.list('WebServer').splitlines()
+    for ws in webservers:
+        ws_name = AdminConfig.showAttribute(ws, 'name')
+        # Get the node this webserver belongs to
+        ws_node = ws.split('/nodes/')[1].split('/')[0]
+        ws_host = AdminConfig.showAttribute(AdminConfig.getid('/Node:'+ws_node+'/'), 'hostName')
+        
+        # Try to get live version via MBean (Requires IHS Admin/NodeAgent to be active)
+        ws_mbean = AdminControl.queryNames('type=WebServer,name=' + ws_name + ',*')
+        if ws_mbean:
+            try:
+                # serverVersion for IHS usually returns "IBM_HTTP_Server/9.0.5.11 (Unix)"
+                raw_ihs = AdminControl.getAttribute(ws_mbean, 'serverVersion')
+                ihs_ver = clean_version(raw_ihs)
+                print("IHS,%s,%s,N/A,%s" % (ws_host, ws_name, ihs_ver))
+            except:
+                print("IHS,%s,%s,N/A,Error" % (ws_host, ws_name))
+        else:
+            # If MBean isn't found, the webserver is likely unmanaged or stopped
+            print("IHS,%s,%s,N/A,Stopped/Unmanaged" % (ws_host, ws_name))
 
 if __name__ == "__main__":
-    get_all_versions()
+    get_inventory()
